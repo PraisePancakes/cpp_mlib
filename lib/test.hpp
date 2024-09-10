4d02c3c74633db85c20c9eee99b8f6fa2a78361f
PraisePancakes <100659653+PraisePancakes@users.noreply.github.com>
2024-09-10 02:23:19 -0400

    refactored test api

--- lib/test.hpp
+++ lib/test.hpp
@@ -13,25 +13,27 @@ enum class TestID
 };
 
 template <typename __INPUT, typename __EXPOUTPUT>
-class ITest
+class MTest
 {
 protected:
 public:
-    std::string _ITest_subject;
+    std::string _ITest_title;
     __INPUT _ITest_input;
     __EXPOUTPUT _ITest_expected;
     TestID _ITest_id;
     bool _ITest_passed;
     std::function<void()> _ITest_operation;
 
-    ITest(const std::string &__subj__, const TestID __id__, __INPUT __inp__, __EXPOUTPUT __expect__)
+    MTest(const std::string &__title__, const TestID __id__, __INPUT __inp__, __EXPOUTPUT __expect__)
     {
-        this->_ITest_subject = __subj__;
+
+        this->_ITest_title = "[TEST][" + __title__ + "]";
         this->_ITest_id = __id__;
         this->_ITest_input = __inp__;
 
         this->_ITest_expected = __expect__;
     };
+
     void fail()
     {
         this->_ITest_passed = false;
@@ -48,7 +50,20 @@ public:
     }
     // pass lambda here
 
-    virtual void set_operation(std::function<void()> __functor__) = 0;
+    void execute()
+    {
+        if (this->_ITest_operation) // Check if operation is set
+        {
+            this->_ITest_operation();
+            this->determine();
+        }
+    }
+
+    void set_operation(std::function<void()> __functor__)
+    {
+        std::cout << this->_ITest_title;
+        this->_ITest_operation = __functor__;
+    };
     void determine()
     {
         if (this->_ITest_expected == this->_ITest_input)
@@ -60,31 +75,5 @@ public:
             this->fail();
         };
     }
-    ~ITest() {};
-};
-
-// VecTest<mlib::vec<int>, mlib::vec<int>, mlib::vec<int>> vtest("PUSH_BACK", {0, 2, 3, 4}, {0, 2, 3, 4}, {0, 2, 3, 4, 5});
-//
-
-template <typename __INPUT, typename __EXPOUTPUT>
-class VecTest : public ITest<__INPUT, __EXPOUTPUT>
-{
-
-public:
-    VecTest(const std::string &__operation__, __INPUT __v_in__, __EXPOUTPUT __v_expect__) : ITest<__INPUT, __EXPOUTPUT>("[TEST][VECTOR][" + __operation__ + "]", TestID::TID_VECTOR, __v_in__, __v_expect__) {};
-
-    void set_operation(std::function<void()> __functor__) override
-    {
-        std::cout << this->_ITest_subject;
-        this->_ITest_operation = __functor__;
-    };
-
-    void execute()
-    {
-        if (this->_ITest_operation) // Check if operation is set
-        {
-            this->_ITest_operation();
-            this->determine();
-        }
-    }
+    ~MTest() {};
 };
--- main.cpp
+++ main.cpp
@@ -5,17 +5,17 @@
 
 int main()
 {
-  VecTest<mlib::vec<int>, mlib::vec<int>> v("PUSH_BACK", {0, 1, 2, 3}, {0, 1, 2, 3, 4});
-  VecTest<mlib::vec<int>, mlib::vec<int>> v2("POP", {0, 1, 2, 3}, {0, 1, 2});
 
-  v.set_operation([&v]()
-                  { v._ITest_input.push_back(4); });
+  // test on the spot
+
+  MTest<mlib::vec<int>, mlib::vec<int>> v2("VECTOR POP", TestID::TID_VECTOR, {0, 1, 2, 3}, {0, 1, 2});
+
   v2.set_operation([&v2]()
                    { v2._ITest_input.pop_back(); });
-  v.execute();
+
   v2.execute();
-  std::cout << v.success();
-  std::cout << v2.success();
+
+  std::cout << v2.success() << std::endl;
 
   return 0;
 };
