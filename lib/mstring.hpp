ffd8a9e1fcd2f9319c504775b8431642c988df73
PraisePancakes <100659653+PraisePancakes@users.noreply.github.com>
2024-09-13 00:51:46 -0400

    re implemented string

--- lib/mstring.hpp
+++ lib/mstring.hpp
@@ -4,53 +4,89 @@
 
 namespace mlib
 {
+
     class string
     {
-        char *_M_str;
-        size_t _M_s_size = 0;
 
-        size_t _get_size(char *__str__) const
+        struct _underlying_const_alloc
         {
-            size_t i = 0;
-            while (*(__str__ + i) != '\0')
+
+            const char *internal_str;
+            size_t internal_byte_size;
+
+            _underlying_const_alloc()
             {
-                i++;
+
+                this->internal_byte_size = 0;
+                this->internal_str = nullptr;
             }
-            return i;
+
+            _underlying_const_alloc(const char *__str__)
+            {
+                std::cout << __str__;
+                internal_str = __str__;
+                internal_byte_size = sizeof(char) * _M_get_size(__str__);
+            }
+
+            void operator=(const char *__other__)
+            {
+                this->internal_str = __other__;
+                this->internal_byte_size = sizeof(char) * _M_get_size(__other__);
+            }
+
+        private:
+            const size_t _M_get_size(const char *__str__) const
+            {
+                size_t i = 0;
+
+                while (__str__[i] != '\0')
+                {
+                    i++;
+                }
+
+                return i + 1;
+            }
+        };
+
+        _underlying_const_alloc *_M_internal_str;
+        size_t _M_s_size = 0;
+        char *_M_modifiable_str;
+
+        void _copy_to_modifiable(const _underlying_const_alloc &__str__)
+        {
+
+            const size_t str_size = sizeof(char) * __str__.internal_byte_size;
+            std::cout << __str__.internal_str << str_size;
+            _M_modifiable_str = (char *)malloc(str_size);
+            for (size_t i = 0; i < str_size; i++)
+            {
+                _M_modifiable_str[i] = __str__.internal_str[i];
+            }
+
+            _M_s_size = str_size;
         }
 
     public:
-        string(char *__str__)
+        string(const char *__str__)
         {
-            this->_M_str = __str__;
-            _M_s_size = _get_size(__str__);
+
+            _M_internal_str = new _underlying_const_alloc(__str__);
+            _copy_to_modifiable(*_M_internal_str);
         };
 
-        const char *data() const
+        char *data() const
         {
-            return _M_str;
+            return _M_modifiable_str;
         }
 
-        size_t length() const
+        char &operator[](size_t __index__)
         {
-            return _M_s_size;
-        }
-
-        size_t size() const
-        {
-            return _M_s_size;
-        }
-
-        char &operator[](size_t __index__) const
-        {
-            return _M_str[__index__];
+            return _M_modifiable_str[__index__];
         };
 
-        friend std::ostream &
-        operator<<(std::ostream &os, const mlib::string &str)
+        friend std::ostream &operator<<(std::ostream &__os__, const mlib::string &__str__)
         {
-            os << str.data();
-            return os;
+            return __os__ << __str__.data();
         }
     };
 
--- main.cpp
+++ main.cpp
@@ -1,9 +1,15 @@
 #include "lib/mvector.hpp"
 #include "lib/mtest.hpp"
 #include "tests/vector/test_vector.hpp"
+#include "lib/mstring.hpp"
+
 int main()
 {
 
-  test_vector();
+  // test_vector();
+
+  mlib::string s("hello");
+  s[0] = 'a';
+  std::cout << s;
   return 0;
 };
